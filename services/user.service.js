6684197cfd66055eb09504c7cfd0489634543b46
Abdullokh <abdullokh.karimov.2006@gmail.com>
2025-03-13 17:45:20 +0500

    1.2 version

--- services/user.service.js
+++ services/user.service.js
@@ -1,1 +1,22 @@
-const model = require();
+const { getUserById } = require("../controllers/user.controller");
+const models = require("../models/models");
+
+const getAllUsers = async () => {
+  try {
+    const result = await models.find({});
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const result = await models.findById(id);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { getAllUsers, getUserById };
